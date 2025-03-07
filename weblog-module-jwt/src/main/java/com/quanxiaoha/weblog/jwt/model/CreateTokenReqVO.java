c148ac9df15fd478cc58de097ce6dd72bef8b00f
opopop832 <13879834307@163.com>
2025-03-07 17:55:46 +0800

    Add files via upload

---
+++ weblog-module-jwt/src/main/java/com/quanxiaoha/weblog/jwt/model/CreateTokenReqVO.java
@@ -0,0 +1,21 @@
+package com.quanxiaoha.weblog.jwt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: XXX
+ * @url: www.XXX.com.com
+ * @date: 2024-04-19 16:06
+ * @description: TODO
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateTokenReqVO {
+    private String username;
+    private String password;
+}
